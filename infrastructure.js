8374edff310f5cc539c4a267023d3f6eb57f3662
fatimajamilchishti-97 <fatimajamilchishti@gmail.com>
2026-08-01 18:46:45 +0500

    Refactor handler to support CORS and error handling

--- infrastructure.js
+++ infrastructure.js
@@ -1,2 +1,14 @@
 const { INFRASTRUCTURE } = require('../lib/data');
-module.exports = (req, res) => res.status(200).json(INFRASTRUCTURE);
+
+module.exports = async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  
+  if (req.method === 'GET') {
+    res.statusCode = 200;
+    return res.end(JSON.stringify(INFRASTRUCTURE || []));
+  }
+  
+  res.statusCode = 405;
+  return res.end(JSON.stringify({ error: "Method not allowed" }));
+};
