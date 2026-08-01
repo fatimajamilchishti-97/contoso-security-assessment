57579deb580d101ddf24e7aefddab85953fb5e38
fatimajamilchishti-97 <fatimajamilchishti@gmail.com>
2026-08-01 18:46:15 +0500

    Refactor mobile-risks.js to handle CORS and methods

--- mobile-risks.js
+++ mobile-risks.js
@@ -1,2 +1,14 @@
-const { MOBILE_RISKS } = require('../lib/data')
-module.exports = (req, res) => res.status(200).json(MOBILE_RISKS);
+const { MOBILE_RISKS } = require('../lib/data');
+
+module.exports = async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  
+  if (req.method === 'GET') {
+    res.statusCode = 200;
+    return res.end(JSON.stringify(MOBILE_RISKS || []));
+  }
+  
+  res.statusCode = 405;
+  return res.end(JSON.stringify({ error: "Method not allowed" }));
+};
